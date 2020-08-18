41915c694e8caef78486afc10c5c72cc3606bb54
Nicole <nicoleshek315@gmail.com>
2020-08-18 09:13:30 +0200

    connected to mongodb server, save posts in database

--- app.js
+++ app.js
@@ -3,8 +3,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-// Load the full build.
 const lodash = require('lodash');
+const mongoose = require('mongoose');
+mongoose.set('useFindAndModify', false);
+
+mongoose.connect('mongodb://localhost:27017/dailyjournalDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = new mongoose.model('Post', postSchema);
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -12,7 +26,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let posts = [];
+let requestedPostId = "";
 
 app.set('view engine', 'ejs');
 
@@ -21,12 +35,20 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-
 app.get('/', function (req, res) {
-  res.render('home', {
-    homeStartingContent: homeStartingContent,
-    posts: posts
+
+  Post.find({}, function (err, posts) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.render('home', {
+      homeStartingContent: homeStartingContent,
+      posts: posts
+    });
   });
+
 });
 
 app.get('/about', function (req, res) {
@@ -42,36 +64,47 @@ app.get('/compose', function (req, res) {
 // get text input
 app.post('/compose', function (req, res) {
 
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
+  });
 
-  posts.push(post);
+  console.log(req.body.postBody);
+  post.save(function (err) {
+    if (!err) {
+      res.redirect('/');
+    } else {
+      console.log(err);
+    }
+  });
 
-
-  res.redirect('/');
 });
 
-app.get('/posts/:postName', function (req, res) {
-  const requestedTitle = lodash.lowerCase(req.params.postName);
+app.post('/delete', function (req, res) {
 
-
-  posts.forEach(post => {
-    const storedTitle = lodash.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render('post', {
-        postTitle: post.title,
-        postContent: post.content
-      });
-      console.log("Match found");
+  Post.findByIdAndRemove(requestedPostId, function (err) {
+    if (err) {
+      console.log(err);
     } else {
-      console.log("not a match")
+      res.redirect("/");
     };
   });
 });
 
+app.get("/posts/:postId", function (req, res) {
+
+  requestedPostId = req.params.postId;
+
+  Post.findOne({
+    _id: requestedPostId
+  }, function (err, post) {
+    res.render("post", {
+      postTitle: post.title,
+      postContent: post.content
+    });
+  });
+
+});
 
 
 app.get("/contact", function (req, res) {
@@ -90,6 +123,6 @@ app.get("/contact", function (req, res) {
 
 
 
-app.listen(5000, function () {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
